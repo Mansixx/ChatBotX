c7c7479c7e7c825aa53844b62919061259d850d3
Ashutosh-Alhodood <ashutosh@alhodood.com>
2026-02-01 20:40:13 +0530

    Fixed

--- ui/src/components/ChatUI.jsx
+++ ui/src/components/ChatUI.jsx
@@ -9,9 +9,9 @@ export default function ChatUI() {
   const bottomRef = useRef(null)
 
 
-const RASA_URL = window.location.origin.replace("4173", "5005")
+// const RASA_URL = window.location.origin.replace("4173", "5005")
 // const RASA_URL = "http://localhost:5005"
-  // const RASA_URL = "http://13.48.85.237:5005"
+  const RASA_URL = "http://13.48.85.237:5005"
 
   // Test Rasa connection on mount
   useEffect(() => {
